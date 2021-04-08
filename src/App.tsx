83138617f34722c57057f5d40fa554fd5cc73075
WhiteKr <hanjisu2215g@gmail.com>
2021-04-09 00:58:18 +0900

    Added DropBox on App

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './styles/App.css';
 
 import Header from './components/Header';
+import DropBox from './components/DropBox';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
 				<Header />
-
+				
 				<div className="gradation">
+					<div className="dropBoxInApp">
+						<DropBox />
+					</div>
 				</div>
 			</div>
 		)
