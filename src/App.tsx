82960cb077d2638b0bf15166537aa4735221fdb4
WhiteKr <hanjisu2215g@gmail.com>
2021-04-09 00:46:21 +0900

    Added Header component on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './styles/App.css';
 
+import Header from './components/Header';
+
 class App extends React.Component {
 	render() {
 		return (
-			<>
-			</>
+			<div className="App">
+				<Header />
+			</div>
 		)
 	}
 }
